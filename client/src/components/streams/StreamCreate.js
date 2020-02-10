eec3c2e8db0a56e9c14b101d100c0b4ef444c17c
Somat Thakali <sthakal000@citymail.cuny.edu>
2020-02-10 18:06:52 -0500

    Created stream action

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
 
 class StreamCreate extends React.Component {
   renderError({ error, touched }) {
     if (touched && error) {
       return (
         <div className="ui error message">
-          <div ckassName="header">{error}</div>
+          <div className="header">{error}</div>
         </div>
       );
     }
@@ -25,8 +27,7 @@ class StreamCreate extends React.Component {
     );
   };
   onSubmit = formValues => {
-    console.log("formValues", formValues);
-    // this.props.onSubmit(formValues);
+    this.props.createStream(formValues);
   };
   render() {
     return (
@@ -56,7 +57,10 @@ const validate = formValues => {
   }
   return errors;
 };
-export default reduxForm({
+
+const formWrapped = reduxForm({
   form: "streamCreate",
   validate
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
